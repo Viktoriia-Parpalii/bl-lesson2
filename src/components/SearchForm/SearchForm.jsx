aa997dbd6030c137cd8432b599f99fa60effa55c
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-10-08 14:44:49 +0200

    update galery component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,37 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  handleInput = e => {
+    this.setState({ query: e.currentTarget.value.toLowerCase().trim() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.query);
+
+    this.setState({ query: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          onChange={this.handleInput}
+          value={this.state.query}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,36 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    loading: false,
+    error: null,
+  };
+
+  componentDidMount() {
+    this.getPhotos('cat', 1);
+  }
+  getPhotos = async (query, page) => {
+    if (!query) {
+      return;
+    }
+    this.setState({ loading: true });
+    try {
+      const data = await ImageService.getImages(query, page);
+      console.log(data);
+    } catch (error) {}
+  };
+
+  onHandleSubmit = value => {
+    this.setState({ query: value });
+  };
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.onHandleSubmit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
